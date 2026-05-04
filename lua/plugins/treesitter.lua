45ac5d45039007e0013b1ce19349259d40d6641d
Callum Wellwood-Kane <business@callumwk.com>
2026-05-04 10:39:01 +1000

    fixed treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
-vim.pack.add({ { src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = "master" } })
+vim.pack.add({ { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" } })
 
-require('nvim-treesitter.configs').setup({
-	ensure_installed = { 'svelte', 'typescript' },
-	highlight = { enable = true }
-})
+-- require("nvim-treesitter").setup({
+-- 	ensure_installed = { "svelte", "typescript" },
+-- 	highlight = { enable = true },
+-- })
